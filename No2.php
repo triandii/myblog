548e46230b10a2838f5da6626f36a569b488ef16
Triandi <86074088+triandii@users.noreply.github.com>
2021-11-01 22:03:04 +0700

    Add files via upload

---
+++ No2.php
@@ -0,0 +1,9 @@
+<?php
+for($bil = 1; $bil <= 5; $bil++){
+    if( ($bil % 2) == 0){
+        echo "$bil Adalah Bilangan Genap<br>";
+    }else {
+		echo "$bil Adalah Bilangan Ganjil<br>";
+	}	
+}
+?>
